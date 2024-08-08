09cb640de4b01008471c724e746c3757a5cd9c14
Abby Duke <t-aduke@microsoft.com>
2024-08-08 11:00:53 -0700

    updated client logging to use ForContext instead of PushProperty

--- clientLogger/ClientLogger.cs
+++ clientLogger/ClientLogger.cs
@@ -5,7 +5,6 @@ using Serilog.Context;
 
 namespace AKSMiddleware;
 
-// Client-side interceptor for logging
 public class ClientLoggerInterceptor : Interceptor
 {
     private readonly Serilog.ILogger _logger;
@@ -22,12 +21,6 @@ public class ClientLoggerInterceptor : Interceptor
     {
         DateTime start = DateTime.Now;
         
-        LogContext.PushProperty(Constants.SystemTag[0], Constants.SystemTag[1]);
-        LogContext.PushProperty(Constants.ComponentFieldKey, Constants.KindClientFieldValue);
-        LogContext.PushProperty(Constants.MethodFieldKey, context.Method.Name);
-        LogContext.PushProperty(Constants.MethodTypeFieldKey, context.Method.Type.ToString().ToLower());
-        LogContext.PushProperty(Constants.ComponentFieldKey, Constants.KindClientFieldValue);
-        
         var headers = context.Options.Headers;
         string requestId = string.Empty;
         if (headers != null)
@@ -38,37 +31,40 @@ public class ClientLoggerInterceptor : Interceptor
                 requestId = headerValue;
             }
         }
-        LogContext.PushProperty(Constants.RequestIDLogKey, requestId);
 
         string serviceName = ExtractServiceName(context.Method.FullName);
-        LogContext.PushProperty(Constants.ServiceFieldKey, serviceName);
+
+        var logger = _logger.ForContext(Constants.ServiceFieldKey, serviceName)
+                            .ForContext(Constants.RequestIDLogKey, requestId)
+                            .ForContext(Constants.SystemTag[0], Constants.SystemTag[1])
+                            .ForContext(Constants.ComponentFieldKey, Constants.KindClientFieldValue)
+                            .ForContext(Constants.MethodFieldKey, context.Method.Name)
+                            .ForContext(Constants.MethodTypeFieldKey, context.Method.Type.ToString().ToLower())
+                            .ForContext(Constants.ComponentFieldKey, Constants.KindClientFieldValue);
 
         var response = continuation(request, context);
-
-        Task.Run(() => HandleResponse(response, start));
-
+        Task.Run(() => HandleResponse(response, start, logger));
         return response;
     }
 
-    private async Task<TResponse> HandleResponse<TResponse>(AsyncUnaryCall<TResponse> call, DateTime start)
+    private async Task<TResponse> HandleResponse<TResponse>(AsyncUnaryCall<TResponse> call, DateTime start, Serilog.ILogger logger)
     {
         try
         {
             var response = await call.ResponseAsync;
-
             var duration = DateTime.Now - start;
-            LogContext.PushProperty(Constants.TimeMsKey, duration.TotalMilliseconds);
-            LogContext.PushProperty(Constants.StartTimeKey, start.ToString("yyyy-MM-ddTHH:mm:sszzz"));
-            
             var status = call.GetStatus();
-            LogContext.PushProperty(Constants.StatusCodeKey, status.StatusCode);
 
-            _logger.Information("finished call");
+            logger = logger.ForContext(Constants.StatusCodeKey, status.StatusCode)
+                           .ForContext(Constants.TimeMsKey, duration.TotalMilliseconds)
+                           .ForContext(Constants.StartTimeKey, start.ToString("yyyy-MM-ddTHH:mm:sszzz"));
+
+            logger.Information("finished call");
             return response;
         }
         catch (Exception ex)
         {
-            _logger.Error(ex, $"Call error: {ex.Message}");
+            logger.Error(ex, $"Call error: {ex.Message}");
             throw;
         }
     }
