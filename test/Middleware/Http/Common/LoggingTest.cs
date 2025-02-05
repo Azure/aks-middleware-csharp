7a98e3e25bab5fb1c6807c9bbb201f4dfe58972c
tomabraham <tomabraham@microsoft.com>
2025-02-05 14:02:55 -0800

    reset http logging changes

--- test/Middleware/Http/Common/LoggingTest.cs
+++ test/Middleware/Http/Common/LoggingTest.cs
@@ -23,29 +23,28 @@ public class LoggingTests
         logOutput = new StringWriter();
         logger = new LoggerConfiguration()
             .WriteTo.TextWriter(logOutput, LogEventLevel.Information, template)
-            .WriteTo.Console(outputTemplate: template)
             .CreateLogger();
     }
 
-    // // This test validates that GET requests with a nested resource are logged as a READ operation.
-    // [Fact]
-    // public void Get_WithNestedResourcelogsReadOperation()
-    // {
-    //     var url = "https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts/account_name?api-version=version";
-    //     var request = new HttpRequestMessage(HttpMethod.Get, url);
+    // This test validates that GET requests with a nested resource are logged as a READ operation.
+    [Fact]
+    public void Get_WithNestedResourcelogsReadOperation()
+    {
+        var url = "https://management.azure.com/subscriptions/sub_id/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts/account_name?api-version=version";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-    //     var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
-    //     Logging.LogRequest(p);
+        var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
+        Logging.LogRequest(p);
 
-    //     var logString = logOutput.ToString();
-    //     Assert.Contains("GET Microsoft.Storage/storageAccounts - READ", logString);
-    // }
+        var logString = logOutput.ToString();
+        Assert.Contains("GET storageaccounts - READ", logString);
+    }
 
     // This test validates that GET requests with a top-level resource are logged as a LIST operation.
     [Fact]
     public void Get_WithTopLevelResourcelogsListOperation()
     {
-        var url = "https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups?api-version=version";
+        var url = "https://management.azure.com/subscriptions/sub_id/resourceGroups?api-version=version";
         var request = new HttpRequestMessage(HttpMethod.Get, url);
 
         var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
@@ -55,93 +54,92 @@ public class LoggingTests
         Assert.Contains("GET resourcegroups - LIST", logString);
     }
 
-    // // // This test validates that non-GET requests do not include any operation type in the log.
-    // [Fact]
-    // public void NonGetMethod_NoOperationType()
-    // {
-    //     var url = "https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg_name/providers/Microsoft.Storage/storageAccounts?api-version=version";
-    //     var request = new HttpRequestMessage(HttpMethod.Post, url);
+    // // This test validates that non-GET requests do not include any operation type in the log.
+    [Fact]
+    public void NonGetMethod_NoOperationType()
+    {
+        var url = "https://management.azure.com/subscriptions/sub_id/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts?api-version=version";
+        var request = new HttpRequestMessage(HttpMethod.Post, url);
 
-    //     var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
-    //     Logging.LogRequest(p);
+        var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
+        Logging.LogRequest(p);
 
-    //     var logString = logOutput.ToString();
-    //     Assert.Contains("POST Microsoft.Storage/storageAccounts", logString);
-    //     Assert.DoesNotContain("- LIST", logString);
-    //     Assert.DoesNotContain("- READ", logString);
-    // }
+        var logString = logOutput.ToString();
+        Assert.Contains("POST storageaccounts", logString);
+        Assert.DoesNotContain("- LIST", logString);
+        Assert.DoesNotContain("- READ", logString);
+    }
 
-//     // This test validates that AzCore pipeline requests are logged without an operation type.
-//     [Fact]
-//     public void AzCoreRequest_NoOperationType()
-//     {
-//         var clientOptions = ArmPolicy.GetDefaultArmClientOptions(logger);
-//         var pipeline = HttpPipelineBuilder.Build(clientOptions);
-//         var req = pipeline.CreateRequest();
-//         req.Method = RequestMethod.Post;
-//         req.Uri.Reset(new Uri("https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg_name/providers/Microsoft.Storage/storageAccounts?api-version=version"));
+    // This test validates that AzCore pipeline requests are logged without an operation type.
+    [Fact]
+    public void AzCoreRequest_NoOperationType()
+    {
+        var clientOptions = ArmPolicy.GetDefaultArmClientOptions(logger);
+        var pipeline = HttpPipelineBuilder.Build(clientOptions);
+        var req = pipeline.CreateRequest();
+        req.Method = RequestMethod.Post;
+        req.Uri.Reset(new Uri("https://management.azure.com/subscriptions/sub_id/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts?api-version=version"));
 
-//         var p = new LogRequestParams(logger, DateTime.UtcNow, req, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
-//         Logging.LogRequest(p);
+        var p = new LogRequestParams(logger, DateTime.UtcNow, req, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
+        Logging.LogRequest(p);
 
-//         var logString = logOutput.ToString();
-//         Assert.Contains("POST Microsoft.Storage/storageAccounts", logString);
-//     }
+        var logString = logOutput.ToString();
+        Assert.Contains("POST storageaccounts", logString);
+    }
 
-//     // This test validates that a malformed URL is logged in its entirety.
-//     [Fact]
-//     public void MalformedUrllogsEntireUrl()
-//     {
-//         var url = "https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg_name/providers/Microsoft.Storage/storageAccounts/account_name/api-version=version";
-//         var request = new HttpRequestMessage(HttpMethod.Get, url);
+    // This test validates that a malformed URL is logged in its entirety.
+    [Fact]
+    public void MalformedUrllogsEntireUrl()
+    {
+        var url = "https://management.azure.com/subscriptions/sub_id/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts/account_name/api-version=version";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-//         var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
-//         Logging.LogRequest(p);
+        var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
+        Logging.LogRequest(p);
 
-//         var logString = logOutput.ToString();
-//         Assert.Contains("GET https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg_name/providers/Microsoft.Storage/storageAccounts/account_name/api-version=version", logString);
-//     }
+        var logString = logOutput.ToString();
+        Assert.Contains("GET https://management.azure.com/subscriptions/sub_id/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts/account_name/api-version=version", logString);
+    }
 
-//     // This test validates that an unrecognized resource type is logged in its entirety.
-//     [Fact]
-//     public void ResourceTypeNotFoundlogsEntireUrl()
-//     {
-//         var url = "https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/customResourceGroup/resource_name/providers/Microsoft.Storage/customResource/resource_name?api-version=version";
-//         var request = new HttpRequestMessage(HttpMethod.Get, url);
+    // This test validates that an unrecognized resource type is logged in its entirety.
+    [Fact]
+    public void ResourceTypeNotFoundlogsEntireUrl()
+    {
+        var url = "https://management.azure.com/subscriptions/sub_id/customResourceGroup/resource_name/providers/Microsoft.Storage/customResource/resource_name?api-version=version";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-//         var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
-//         Logging.LogRequest(p);
+        var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
+        Logging.LogRequest(p);
 
-//         var logString = logOutput.ToString();
-//         Assert.Contains("GET https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/customResourceGroup/resource_name/providers/Microsoft.Storage/customResource/resource_name - READ", logString);
-//     }
+        var logString = logOutput.ToString();
+        Assert.Contains("GET https://management.azure.com/subscriptions/sub_id/customResourceGroup/resource_name/providers/Microsoft.Storage/customResource/resource_name - READ", logString);
+    }
 
-//     // This test validates that multiple query parameters are recognized and logged properly.
-//     [Fact]
-//     public void MultipleQueryParameterslogsProperly()
-//     {
-//         var url = "https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg_name/providers/Microsoft.Storage/storageAccounts?api-version=version&param1=value1&param2=value2";
-//         var request = new HttpRequestMessage(HttpMethod.Get, url);
+    // This test validates that multiple query parameters are recognized and logged properly.
+    [Fact]
+    public void MultipleQueryParameterslogsProperly()
+    {
+        var url = "https://management.azure.com/subscriptions/sub_id/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts?api-version=version&param1=value1&param2=value2";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-//         var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
-//         Logging.LogRequest(p);
+        var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
+        Logging.LogRequest(p);
 
-//         var logString = logOutput.ToString();
-//         Assert.Contains("GET Microsoft.Storage/storageAccounts - LIST", logString);
-//     }
+        var logString = logOutput.ToString();
+        Assert.Contains("GET storageaccounts - LIST", logString);
+    }
 
-//     // This test validates that when no query parameters are present, the entire URL is logged.
-//     [Fact]
-//     public void ZeroQueryParameterslogsEntireUrl()
-//     {
-//         var url = "https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourcegroups/rg_name/providers/Microsoft.Storage/storageAccounts";
-//         var request = new HttpRequestMessage(HttpMethod.Get, url);
+    // This test validates that when no query parameters are present, the entire URL is logged.
+    [Fact]
+    public void ZeroQueryParameterslogsEntireUrl()
+    {
+        var url = "https://management.azure.com/subscriptions/sub_id/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
 
-//         var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
-//         Logging.LogRequest(p);
+        var p = new LogRequestParams(logger, DateTime.UtcNow, request, new HttpResponseMessage { StatusCode = System.Net.HttpStatusCode.OK }, null!);
+        Logging.LogRequest(p);
 
-//         var logString = logOutput.ToString();
-//         Assert.Contains("GET https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts", logString);
-//     }
-// }
+        var logString = logOutput.ToString();
+        Assert.Contains("GET https://management.azure.com/subscriptions/sub_id/resourceGroups/rg_name/providers/Microsoft.Storage/storageAccounts", logString);
+    }
 }
